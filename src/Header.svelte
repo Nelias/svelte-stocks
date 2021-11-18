888f38d62bff1f7b3327ea1eef9a3d9dc6b06afb
Michał Antropik <michal.antropik@gmail.com>
2021-11-18 14:33:35 +0100

    add brain.js library

---
+++ src/Header.svelte
@@ -0,0 +1,14 @@
+<style>
+  header {
+    display: block;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    background: #eee;
+    padding: 12px;
+  }
+</style>
+
+<header>
+  <nav><a href="/#"> Recent Debuts </a></nav>
+</header>
