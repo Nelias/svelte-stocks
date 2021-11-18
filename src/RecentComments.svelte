34c3f24b3b5717c50395c1ea33e5be6096f7d808
Michał Antropik <michal.antropik@gmail.com>
2021-11-18 19:09:15 +0100

    svelte can not handle the module.exports and rollup.js is not efficient at all

--- src/RecentComments.svelte
+++ src/RecentComments.svelte
@@ -1,13 +1,38 @@
 <script>
+  // import { recurrent } from "brain.js/dist/src";
   export let commentsTitle;
   export let commentsList;
+
+  // let network;
+
+  // console.log(recurrent);
+
+  // network.train([
+  //   { input: "rising", output: "positive" },
+  //   { input: "bored", output: "negative" },
+  //   { input: ":) ;) :D", output: "positive" },
+  //   { input: "we can't have nice things", output: "negative" },
+  //   { input: "tempted", output: "negative" },
+  //   { input: "happy", output: "positive" },
+  //   { input: "good", output: "positive" },
+  //   { input: "congratulations", output: "positive" },
+  //   { input: "sucks", output: "negative" },
+  //   { input: "bad", output: "negative" },
+  // ]);
+
+  // console.log(network.run("this is good"));
 </script>
 
 <h3>{commentsTitle}</h3>
 <div>
   {#if commentsList}
     {@html JSON.parse(JSON.stringify(commentsList)).comments.map(
-      (comment) => comment.content
+      (comment) =>
+        comment.content +
+        ` This comment is: ${
+          "LSTM"
+          // network.run(comment.content)
+        }`
     )}
   {/if}
 </div>
